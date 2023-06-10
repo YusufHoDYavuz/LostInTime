90574111eb5d2e55c3c76706d423463f27c7dc4a
Yusuf <93403926+YusufHoDYavuz@users.noreply.github.com>
2023-06-10 19:22:26 +0300

    add: crouch animation and system
    need bug fix animation transition

--- Assets/Yusuf/Scripts/PlayerController.cs
+++ Assets/Yusuf/Scripts/PlayerController.cs
@@ -5,22 +5,34 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //COMPONENTS
+    private CharacterController characterController;
+    private Transform camera;
+    private Animator animator;
+    
+    [Header("Movement")]
     [SerializeField] private float playerSpeed;
     [SerializeField] private float turnSmoothTime;
     private float turnSmoothVelocity;
+    private Vector3 direction;
     
-    private CharacterController characterController;
-    
-    private Transform camera;
-
-    private Animator animator;
+    [Header("Crouch")]
+    [SerializeField] private float crouchHeight;
+    [SerializeField] private Vector3 crouchHeightPosition;
+    private float originalHeight;
+    private Vector3 originalCrouchHeightPosition;
+    private bool isCrouching = false;
 
     private void Start()
     {
         characterController = GetComponent<CharacterController>();
         animator = transform.GetChild(0).GetComponent<Animator>();
         camera = Camera.main.transform;
-        Debug.Log(transform.GetChild(0).name);
+        
+        //CROUCH
+        originalHeight = characterController.height;
+        originalCrouchHeightPosition = characterController.center;
+
         //Remove cursor
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
@@ -28,9 +40,34 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        Movement();
+        
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            ToggleCrouch();
+        }
+
+        if (direction.magnitude >= 0.1f && isCrouching)
+        {
+            animator.SetBool("isCrouchWalk" , true);
+            animator.SetBool("isCrouch", false);
+        }
+        else
+        {
+            animator.SetBool("isCrouchWalk" , false);
+        }
+
+        if (direction.magnitude < 0.1f && isCrouching)
+        {
+            animator.SetBool("isCrouch", true);
+        }
+    }
+
+    private void Movement()
+    {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        direction = new Vector3(horizontal, 0f, vertical).normalized;
 
         if (direction.magnitude >= 0.1f)
         {
@@ -53,6 +90,28 @@ public class PlayerController : MonoBehaviour
         {
             animator.SetBool("isWalk",false);
         }
-        
     }
+    
+    private void ToggleCrouch()
+    {
+        if (isCrouching)
+        {
+            // Not crouching
+            characterController.height = originalHeight;
+            characterController.center = originalCrouchHeightPosition;
+            animator.SetBool("isCrouch", false);
+            isCrouching = false;
+        }
+        else
+        {
+            // Crouching
+            characterController.height = crouchHeight;
+            characterController.center = crouchHeightPosition;
+            animator.SetBool("isWalk", false);
+            animator.SetBool("isCrouchWalk", true);
+            animator.SetBool("isCrouch", true);
+            isCrouching = true;
+        }
+    }
+
 }
