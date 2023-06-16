6ec5df223ed3863f1fa2a56031c213684cadb5f7
Yusuf <93403926+YusufHoDYavuz@users.noreply.github.com>
2023-06-16 18:30:22 +0300

    add: time pass

---
+++ Assets/Yusuf/Scripts/TimePass.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+using DG.Tweening;
+
+public class TimePass : MonoBehaviour
+{
+    [SerializeField] private float shakeTime;
+    [SerializeField] private float shakeScale;
+    
+    [SerializeField] private GameObject teleportPrefab;
+
+    private CinemachineFreeLook cinemachineFreeLook;
+    private Transform firstFollowTransform;
+
+    private void Start()
+    {
+        cinemachineFreeLook = GetComponent<CinemachineFreeLook>();
+        firstFollowTransform = cinemachineFreeLook.Follow;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            cinemachineFreeLook.Follow = null;
+            ShakeKamera();
+        }
+    }
+
+    public void ShakeKamera()
+    {
+        Instantiate(teleportPrefab, firstFollowTransform.position, Quaternion.identity);
+        transform.DOShakePosition(shakeTime, shakeScale).OnComplete(() =>
+        {
+            cinemachineFreeLook.Follow = firstFollowTransform;
+        });
+    }
+}
