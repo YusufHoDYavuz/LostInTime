1b033ff45577e68215c2ed77ddeeca8f1da05773
Yusuf Yavuz <yusufyavuzcon@gmail.com>
2023-10-16 21:36:32 +0300

    Add: Knowledge UI bar with animation

--- Assets/Yusuf/Scripts/Singleton.cs
+++ Assets/Yusuf/Scripts/Singleton.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using TMPro;
 
 public class Singleton : MonoBehaviour
 {
@@ -16,6 +18,9 @@ public class Singleton : MonoBehaviour
     public bool gameFinished;
     public bool[] pastPuzzlesScrollCount = { false, false, false, false };
     public bool rotationFinished;
+
+    public RectTransform knowledgeUI;
+    public TextMeshProUGUI knowledgeText;
 
     private void Awake()
     {
@@ -58,5 +63,21 @@ public class Singleton : MonoBehaviour
         });
         yield return new WaitForSeconds(waitTime);
         gemsParentUI.transform.DOLocalMove(new Vector3(0, 600, 0), 0.25f);
+    }
+
+    public void SetKnowledge(string knowledgeText)
+    {
+        StartCoroutine(KnowledgeTextDelay(knowledgeText));
+
+        Sequence knowledgeSeq = DOTween.Sequence();
+        knowledgeSeq.Append(knowledgeUI.DOMoveX(-500, 0.25f));
+        knowledgeSeq.AppendInterval(0.25f);
+        knowledgeSeq.Append(knowledgeUI.DOMoveX(25, 0.25f));
+    }
+
+    private IEnumerator KnowledgeTextDelay(string knowledgeText)
+    {
+        yield return new WaitForSeconds(0.25f);
+        this.knowledgeText.text = knowledgeText;
     }
 }
